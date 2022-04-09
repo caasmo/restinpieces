1c26372bbbbe06213d5634f23976e82cba6426f5
caasmo <226161+caasmo@users.noreply.github.com>
2022-04-09 11:50:42 +0200

    proof of concept

---
+++ cmd/restinpieces/main.go
@@ -0,0 +1,109 @@
+package main
+
+import (
+    "database/sql"
+    "encoding/json"
+    //"errors"
+    "fmt"
+    "log"
+    "net/http"
+    "time"
+
+    "github.com/julienschmidt/httprouter"
+    "github.com/justinas/alice"
+)
+
+// We could also put *httprouter.Router in a field to not get access to the original methods (GET, POST, etc. in uppercase)
+type router struct {
+    *httprouter.Router
+}
+
+func (r *router) Get(path string, handler http.Handler) {
+    r.GET(path, wrapHandler(handler))
+}
+
+// httprouter
+func wrapHandler(h http.Handler) httprouter.Handle {
+    return func(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+        //context.Set(r, "params", ps)
+        h.ServeHTTP(w, r)
+    }
+}
+
+
+func NewRouter() *router {
+    return &router{httprouter.New()}
+}
+
+type appContext struct {
+    db *sql.DB
+}
+
+
+func loggingHandler(next http.Handler) http.Handler {
+    fn := func(w http.ResponseWriter, r *http.Request) {
+        t1 := time.Now()
+        next.ServeHTTP(w, r)
+        t2 := time.Now()
+        log.Printf("[%s] %q %v\n", r.Method, r.URL.String(), t2.Sub(t1))
+    }
+
+    return http.HandlerFunc(fn)
+}
+
+func (c *appContext) adminHandler(w http.ResponseWriter, r *http.Request) {
+
+    user := "testuser"
+    //user := context.Get(r, "user")
+    // Maybe other operations on the database
+    json.NewEncoder(w).Encode(user)
+}
+
+func (c *appContext) authHandler(next http.Handler) http.Handler {
+    fn := func(w http.ResponseWriter, r *http.Request) {
+        authToken := r.Header.Get("Authorization")
+        //user, err := map[string]interface{}{}, errors.New("test")
+        //user := authToken
+        // user, err := getUser(c.db, authToken)
+        log.Println(authToken)
+
+        // if return in middleware, no next, chain stopped
+        //if err != nil {
+        //    http.Error(w, http.StatusText(401), 401)
+        //    return
+        //}
+
+        // TODO communication betwwen handlers
+        //context.Set(r, "user", user)
+        next.ServeHTTP(w, r)
+    }
+
+    return http.HandlerFunc(fn)
+}
+
+func (c *appContext) teaHandler(w http.ResponseWriter, r *http.Request) {
+    //params := context.Get(r, "params").(httprouter.Params)
+    //log.Println(params.ByName("id"))
+    // tea := getTea(c.db, params.ByName("id"))
+    json.NewEncoder(w).Encode(nil)
+}
+
+
+func aboutHandler(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprintf(w, "You are on the about page.")
+}
+
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprintf(w, "Welcome!")
+}
+
+func main() {
+    appC := appContext{nil}
+    commonHandlers := alice.New(loggingHandler)
+    router := NewRouter()
+    router.Get("/admin", commonHandlers.Append(appC.authHandler).ThenFunc(appC.adminHandler))
+    router.Get("/about", commonHandlers.ThenFunc(aboutHandler))
+    router.Get("/", commonHandlers.ThenFunc(indexHandler))
+    router.Get("/teas/:id", commonHandlers.ThenFunc(appC.teaHandler))
+    http.ListenAndServe(":8080", router)
+}
---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/caasmo/restinpieces
+
+go 1.18
+
+require (
+	github.com/julienschmidt/httprouter v1.3.0
+	github.com/justinas/alice v1.2.0
+)
