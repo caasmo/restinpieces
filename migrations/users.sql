b175bfca014fda8444b07aa49f9c20c64afcb86b
caasmo <226161+caasmo@users.noreply.github.com>
2025-03-15 15:30:57 +0100

    chore: Fix typo and standardize column name in users.sql

--- migrations/users.sql
+++ migrations/users.sql
@@ -2,7 +2,7 @@
 -- When using the BOOLEAN type in SQLite, the data
 -- is stored as 0 or 1 (as INTEGER), and this is the standard SQLite behavior. 
 -- In SQLite, the BOOLEAN type is simply an alias for INTEGER,
--- verified` BOOLEAN DEFAULT FALSE NOT NULL is an aliad for `verified` INTEGER DEFAULT 0 NOT NULL
+-- verified` BOOLEAN DEFAULT FALSE NOT NULL is an alias for `verified` INTEGER DEFAULT 0 NOT NULL
 -- sqlite package like crawshaw will automatically convert go boolean types to integer 0 or 1 (writes)
 CREATE TABLE `users`(
   `avatar` TEXT DEFAULT '' NOT NULL,
@@ -11,7 +11,7 @@ CREATE TABLE `users`(
   `id` TEXT PRIMARY KEY DEFAULT('r'||lower(hex(randomblob(7)))) NOT NULL,
   `name` TEXT DEFAULT '' NOT NULL,
   `password` TEXT DEFAULT '' NOT NULL,
-  `ExternalAuth` TEXT DEFAULT '' NOT NULL,
+  `externalAuth` TEXT DEFAULT '' NOT NULL,
   `created` TEXT NOT NULL DEFAULT (strftime('%Y-%m-%dT%H:%M:%SZ', 'now')),
   `updated` TEXT NOT NULL DEFAULT (strftime('%Y-%m-%dT%H:%M:%SZ', 'now')),
   `verified` BOOLEAN DEFAULT FALSE NOT NULL
