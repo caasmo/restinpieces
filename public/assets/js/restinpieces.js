f3095de411eb4ef9af9f57a5f2fba7be93d6a1f9
caasmo <226161+caasmo@users.noreply.github.com>
2025-03-24 16:10:59 +0100

    feat: Add Restinpieces class with constructor parameters

---
+++ public/assets/js/restinpieces.js
@@ -0,0 +1,11 @@
+
+class Restinpieces {
+    constructor(baseURL = "/", authStore = new LocalAuthStore(), lang = "en-US") {
+        this.baseURL = baseURL;
+        this.lang = lang;
+        //this.authStore = authStore;
+        //this.recordServices = {}; // Cache for record services
+        //this.enableAutoCancellation = true;
+        //this.cancelControllers = {};
+    }
+}
