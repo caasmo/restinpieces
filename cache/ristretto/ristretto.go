d1abb831bdd3f2938e9fcd8661f3a8a2bb0f02b8
caasmo <226161+caasmo@users.noreply.github.com>
2025-04-06 15:07:28 +0200

    refactor: Rename ristretto cache variable for brevity

--- cache/ristretto/ristretto.go
+++ cache/ristretto/ristretto.go
@@ -4,15 +4,15 @@ import (
 	"time"
 
 	"github.com/caasmo/restinpieces/cache"
-	"github.com/dgraph-io/ristretto/v2"
+	ristr "github.com/dgraph-io/ristretto/v2"
 )
 
 type Cache[K comparable, V any] struct {
-	cache *ristretto.Cache
+	c *ristr.Cache
 }
 
 func (rc *Cache[K, V]) Get(key K) (V, bool) {
-	value, found := rc.cache.Get(key)
+	value, found := rc.c.Get(key)
 	if !found {
 		var zero V
 		return zero, false
@@ -21,17 +21,17 @@ func (rc *Cache[K, V]) Get(key K) (V, bool) {
 }
 
 func (rc *Cache[K, V]) Set(key K, value V, cost int64) bool {
-	return rc.cache.Set(key, value, cost)
+	return rc.c.Set(key, value, cost)
 }
 
 func (rc *Cache[K, V]) SetWithTTL(key K, value V, cost int64, ttl time.Duration) bool {
 	// Wait for the item to be processed by the cache
-	success := rc.cache.SetWithTTL(key, value, cost, ttl)
+	success := rc.c.SetWithTTL(key, value, cost, ttl)
 	return success
 }
 
 func New[K comparable, V any]() (cache.Cache[K, V], error) {
-	c, err := ristretto.NewCache(&ristretto.Config{
+	ristretto, err := ristr.NewCache(&ristr.Config{
 		NumCounters: 1e7,     // number of keys to track frequency of (10M)
 		MaxCost:     1 << 30, // maximum cost of cache (1GB)
 		BufferItems: 64,      // number of keys per Get buffer
@@ -40,5 +40,5 @@ func New[K comparable, V any]() (cache.Cache[K, V], error) {
 		return nil, err
 	}
 
-	return &Cache[K, V]{cache: c}, nil
+	return &Cache[K, V]{c: ristretto}, nil
 }
