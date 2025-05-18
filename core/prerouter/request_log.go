01b9fa2b7bad83a3dc632be80236ff65598513c4
caasmo <226161+caasmo@users.noreply.github.com>
2025-05-19 00:26:59 +0200

    feat: add logging for response header writes

--- core/prerouter/request_log.go
+++ core/prerouter/request_log.go
@@ -66,6 +66,7 @@ type responseRecorder struct {
 }
 
 func (r *responseRecorder) WriteHeader(status int) {
+    slog.Info("writing header")
 	r.status = status
 	r.ResponseWriter.WriteHeader(status)
 }
