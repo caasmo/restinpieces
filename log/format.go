fa06c79a2edf90df68355cb27851b932eda8067c
caasmo <226161+caasmo@users.noreply.github.com>
2025-05-27 16:24:14 +0200

    refactor: Remove redundant comments from log formatter methods

--- log/format.go
+++ log/format.go
@@ -20,27 +20,22 @@ func (f *MessageFormatter) WithComponent(name, emoji string) *MessageFormatter {
 	return f
 }
 
-// Fail formats a failure message
 func (f *MessageFormatter) Fail(msg string) string {
 	return fmt.Sprintf("%s %s: ❌%s", f.componentEmoji, f.component, msg)
 }
 
-// Ok formats a success/ok message
 func (f *MessageFormatter) Ok(msg string) string {
 	return fmt.Sprintf("%s %s: 👍 %s", f.componentEmoji, f.component, msg)
 }
 
-// Warn formats a warning message
 func (f *MessageFormatter) Warn(msg string) string {
 	return fmt.Sprintf("%s %s: ⚠️  %s", f.componentEmoji, f.component, msg)
 }
 
-// Start formats a startup/begin message with rocket emoji
 func (f *MessageFormatter) Start(msg string) string {
 	return fmt.Sprintf("%s %s: 🚀 %s", f.componentEmoji, f.component, msg)
 }
 
-// Complete formats a completion message with party popper emoji
 func (f *MessageFormatter) Complete(msg string) string {
 	return fmt.Sprintf("%s %s: 🎉 %s", f.componentEmoji, f.component, msg)
 }
