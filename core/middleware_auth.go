e9c7f5c5a9ef0235d1f5e0962699630742469b58
caasmo <226161+caasmo@users.noreply.github.com>
2025-06-05 16:20:30 +0200

    refactor: Delete redundant auth middleware file

--- core/middleware_auth.go
+++
@@ -1,5 +0,0 @@
-package core
-
-// NOTE: The JwtValidate middleware function has been removed.
-// Its logic is now encapsulated in the App.Authenticate method in core/methods_auth.go.
-// The UserIDKey constant and contextKey type have also been removed as they are no longer needed here.
