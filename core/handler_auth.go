59226e54c9853fec1bd804692bb6037cf4efae13
caasmo <226161+caasmo@users.noreply.github.com>
2025-06-09 01:43:43 +0200

    refactor: update auth handler to use Auth() method

--- core/handler_auth.go
+++ core/handler_auth.go
@@ -20,7 +20,7 @@ func (a *App) RefreshAuthHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	// Authenticate the user using the token from the request
-	user, err, authResp := a.Authenticate(r)
+	user, err, authResp := a.Auth().Authenticate(r)
 	if err != nil {
 		WriteJsonError(w, authResp)
 		return
